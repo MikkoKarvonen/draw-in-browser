d161cf9041db10f3c4e8bacca1aaf9d8aaa40a88
DJMiigii <mkarvonen34@gmail.com>
2017-11-30 20:12:19 +0200

    Draw only when pressing

--- scripts.js
+++ scripts.js
@@ -1,19 +1,24 @@
 var canvas = document.querySelector('#draw');
 var ctx = canvas.getContext('2d');
 
+var drawing = false;
 var lastX = 0;
 var lastY = 0;
 
 function draw(e){
+  if(!drawing) return;
   ctx.strokeStyle="#000000";
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
-  ctx.lineTo(e.x, e.y);
+  ctx.lineTo(e.offsetX, e.offsetY);
 	ctx.stroke();
-  [lastX, lastY] = [e.x, e.y]
+  [lastX, lastY] = [e.offsetX, e.offsetY]
 }
 
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousedown', (e) => {
-	[lastX, lastY] = [e.x, e.y];
+  drawing = true;
+	[lastX, lastY] = [e.offsetX, e.offsetY];
 });
+canvas.addEventListener('mouseup', () => drawing = false);
+canvas.addEventListener('mouseout', () => drawing = false);
